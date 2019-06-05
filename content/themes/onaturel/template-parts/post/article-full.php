17da981737c46f85f205e0920aadea8ca4193eb0
helio-cruz <helio-cruz@hotmail.com>
2019-06-05 21:58:26 +0200

    ajout des articles

---
+++ content/themes/onaturel/template-parts/post/article-full.php
@@ -0,0 +1,6 @@
+<div class="post">
+   <img src="<?php the_post_thumbnail_url(); ?>" alt="...">
+      <div class="text">
+        <h2><?php the_title(); ?></h2>
+      <div><?php the_content(); ?></div>
+</div>
