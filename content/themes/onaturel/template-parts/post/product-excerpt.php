0f342a9f347fdada65ad7849370023dc3adfe5ec
helio-cruz <helio-cruz@hotmail.com>
2019-06-13 00:13:16 +0200

    dinamisation

---
+++ content/themes/onaturel/template-parts/post/product-excerpt.php
@@ -0,0 +1,131 @@
+
+<!-- Start Carousel -->
+<div id="carousel-demo" class="carousel-product text-center">
+  <div class="item-1">
+    <div class="small-carousel-thumbnail text-center">
+      <a href="#" class="product-thumbnail-link">
+        <img src="<?php the_post_thumbnail_url(); ?>" alt="" class="thumbnail img-fluid">
+      </a>
+      <div class="pr-thumbnail-details">
+        <a href="#" class="cart-link">
+          <img src="images/icon/icon.png" alt="" class="cart-icon">
+        </a>
+        <p><?php the_excerpt(); ?></p>
+        <p class="thumbnail-price">Prix : 10€</p>            
+      </div>
+    </div>
+  </div>
+  <div class="item-2">
+    <div class="small-carousel-thumbnail text-center">
+      <a href="#" class="product-thumbnail-link">
+        <img src="<?php the_post_thumbnail_url(); ?>" alt="" class="thumbnail img-fluid">
+      </a>
+      <div class="pr-thumbnail-details">
+        <a href="#" class="cart-link">
+          <img src="images/icon/icon.png" alt="" class="cart-icon">
+        </a>
+        <p><?php the_excerpt(); ?></p>
+        <p class="thumbnail-price">Prix : 10€</p>
+      </div>
+    </div>
+  </div>
+  <div class="item-3">
+    <div class="small-carousel-thumbnail text-center">
+      <a href="#" class="product-thumbnail-link">
+        <img src="<?php the_post_thumbnail_url(); ?>" alt="" class="thumbnail img-fluid">
+      </a>
+      <div class="pr-thumbnail-details">
+        <a href="#" class="cart-link">
+          <img src="images/icon/icon.png" alt="" class="cart-icon">
+        </a>
+        <p><?php the_excerpt(); ?></p>
+        <p class="thumbnail-price">Prix : 10€</p>
+      </div>
+    </div>
+  </div>
+  <div class="item-4">
+    <div class="small-carousel-thumbnail text-center">
+      <a href="#" class="product-thumbnail-link">
+        <img src="<?php the_post_thumbnail_url(); ?>" alt="" class="thumbnail img-fluid">
+      </a>
+      <div class="pr-thumbnail-details">
+        <a href="#" class="cart-link">
+          <img src="images/icon/icon.png" alt="" class="cart-icon">
+        </a>
+        <p><?php the_excerpt(); ?></p>
+        <p class="thumbnail-price">Prix : 10€</p>
+      </div>
+    </div>
+  </div>
+  <div class="item-5">
+    <div class="small-carousel-thumbnail text-center">
+      <a href="#" class="product-thumbnail-link">
+        <img src="<?php the_post_thumbnail_url(); ?>" alt="" class="thumbnail img-fluid">
+      </a>
+      <div class="pr-thumbnail-details">
+        <a href="#" class="cart-link">
+          <img src="images/icon/icon.png" alt="" class="cart-icon">
+        </a>
+        <p><?php the_excerpt(); ?></p>
+        <p class="thumbnail-price">Prix : 10€</p>
+      </div>
+    </div>
+  </div>
+  <div class="item-6">
+    <div class="small-carousel-thumbnail text-center">
+      <a href="#" class="product-thumbnail-link">
+        <img src="<?php the_post_thumbnail_url(); ?>" alt="" class="thumbnail img-fluid">
+      </a>
+      <div class="pr-thumbnail-details">
+        <a href="#" class="cart-link">
+          <img src="images/icon/icon.png" alt="" class="cart-icon">
+        </a>
+        <p><?php the_excerpt(); ?></p>
+        <p class="thumbnail-price">Prix : 10€</p>
+      </div>
+    </div>
+  </div>
+  <div class="item-7">
+    <div class="small-carousel-thumbnail text-center">
+      <a href="#" class="product-thumbnail-link">
+        <img src="<?php the_post_thumbnail_url(); ?>" alt="" class="thumbnail img-fluid">
+      </a>
+      <div class="pr-thumbnail-details">
+        <a href="#" class="cart-link">
+          <img src="images/icon/icon.png" alt="" class="cart-icon">
+        </a>
+        <p><?php the_excerpt(); ?></p>
+        <p class="thumbnail-price">Prix : 10€</p>
+      </div>
+    </div>
+  </div>
+  <div class="item-8">
+    <div class="small-carousel-thumbnail text-center">
+      <a href="#" class="product-thumbnail-link">
+        <img src="<?php the_post_thumbnail_url(); ?>" alt="" class="thumbnail img-fluid">
+      </a>
+      <div class="pr-thumbnail-details">
+        <a href="#" class="cart-link">
+          <img src="images/icon/icon.png" alt="" class="cart-icon">
+        </a>
+        <p><?php the_excerpt(); ?>/p>
+        <p class="thumbnail-price">Prix : 10€</p>
+      </div>
+    </div>
+  </div>
+  <div class="item-9">
+    <div class="small-carousel-thumbnail text-center">
+      <a href="#" class="product-thumbnail-link">
+        <img src="<?php the_post_thumbnail_url(); ?>" alt="" class="thumbnail img-fluid">
+      </a>
+      <div class="pr-thumbnail-details">
+        <a href="#" class="cart-link">
+          <img src="images/icon/icon.png" alt="" class="cart-icon">
+        </a>
+        <p><?php the_excerpt(); ?></p>
+        <p class="thumbnail-price">Prix : 10€</p>
+      </div>
+    </div>
+  </div>
+</div>
+<!-- End Carousel -->
