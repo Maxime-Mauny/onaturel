83da5a05bd88f80f10e57f212275b3067ed0d60a
helio-cruz <helio-cruz@hotmail.com>
2019-06-12 11:38:25 +0200

    inte

--- content/themes/onaturel/public/js/app.js
+++ content/themes/onaturel/public/js/app.js
@@ -63,11 +63,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-<<<<<<< HEAD
-/******/ 	var hotCurrentHash = "f2424210c073db0f47f1";
-=======
-/******/ 	var hotCurrentHash = "ee165a698d41d9e6985d";
->>>>>>> 2c3eeca2044e8478867ec5b89cbf3f7d82981fa5
+/******/ 	var hotCurrentHash = "0f8dfe7911a7bf8edd4a";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
