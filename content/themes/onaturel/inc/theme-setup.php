70e29046bacf89361085ae3e9c202f3ef010bb5c
Emilie17 <emilie.callens17@gmail.com>
2019-06-12 11:35:25 +0200

    carousel de produits

---
+++ content/themes/onaturel/inc/theme-setup.php
@@ -0,0 +1,42 @@
+<?php
+
+if (!function_exists('onaturel_setup')):
+
+    function onaturel_setup()
+    {
+        add_theme_support('title-tag');
+        add_theme_support('post-thumbnails');
+
+        add_theme_support('menus');
+
+        register_nav_menus([
+            'menu_header' => 'Menu de navigation en haut de la page',
+          //  'menu_footer' => 'Menu de navigation en bas de la page',
+             
+        ]);
+
+
+        remove_action( 'wp_head', 'print_emoji_detection_script', 7);
+        remove_action( 'admin_print_scripts', 'print_emoji_detection_script' );
+        remove_action( 'admin_print_styles', 'print_emoji_styles' );
+        remove_action( 'wp_print_styles', 'print_emoji_styles');
+     
+        // Supprime le lien vers Windows Live Editor Manifest
+        remove_action( 'wp_head', 'wlwmanifest_link' );
+     
+        // Supprime le lien RSD + XML
+        remove_action( 'wp_head', 'rsd_link' );
+     
+        // Supprime la meta generator
+        remove_action( 'wp_head', 'wp_generator' );
+     
+        // Supprime les extra feed rss
+        remove_action( 'wp_head', 'feed_links_extra', 3 );
+     
+        // Supprime les feeds des Posts et des Commentaires
+        remove_action( 'wp_head', 'feed_links', 2 );
+    }
+
+endif;
+
+add_action('after_setup_theme', 'onaturel_setup');
